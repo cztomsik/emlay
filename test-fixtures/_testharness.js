7db9c47dc81931da300a9f056c09bdf4b01b832e
Kamil Tomsik <info@tomsik.cz>
2023-03-23 12:27:40 +0100

    add fixtures/harness

---
+++ test-fixtures/_testharness.js
@@ -0,0 +1,41 @@
+const sheet = Object.assign(document.createElement('link'), { rel: 'stylesheet', href: '_styles.css' })
+document.head.appendChild(sheet)
+
+window.onload = () => {
+  // get the test cases
+  const testCases = [...document.querySelectorAll('test-case')]
+
+  // wrap for styling
+  for (tc of testCases) {
+    const wrapper = document.createElement('test-case-wrapper')
+    const pre = document.createElement('pre')
+    pre.textContent = tc.innerHTML.trim().replace(/^ {2}/gm, '')
+
+    tc.replaceWith(wrapper)
+    wrapper.appendChild(tc)
+    tc.after(pre)
+  }
+
+  // get the structure, style and computed layouts for each element in each test case
+  window.fixtures = testCases.map((tc) => {
+    if (tc.children.length !== 1) throw new Error('test cases must have exactly one child')
+
+    const dump = (el) => {
+      return {
+        props: Array.from(el.style).map((k) => [k, el.style[k]]),
+        children: Array.from(el.children).map(dump),
+        layout: [el.offsetLeft, el.offsetTop, el.offsetWidth, el.offsetHeight],
+      }
+    }
+
+    return dump(tc.children[0])
+  })
+
+  // debug
+  let hoverEl = null
+  document.addEventListener('mouseover', (e) => {
+    if (hoverEl) hoverEl.classList.remove('hover')
+    hoverEl = e.target
+    hoverEl.classList.add('hover')
+  })
+}
